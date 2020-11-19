da0bf39a81b499ce0a4d25ba6d9fa2ca89440eb0
Anthony Collichio <collich55@gmail.com>
2020-11-19 11:17:29 -0500

    login and logout work. need to adjust form for sign up, but action itself is functional if one hardcodes the extra params

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
 import GreetingContainer from "./greeting/greeting_container";
+import {Route} from "react-router-dom";
 
 const App = () => (
     <div>
         <header>
             <h1>The Nostalgia Book</h1>
             <GreetingContainer />
+            
         </header>
+        <Route path="/login" component={LogInFormContainer} />
+        <Route path="/signup" component={SignUpFormContainer} />
     </div>
 );
 
 export default App;
+
+
